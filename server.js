bb28f46bf93ab722a5935b9db860f1300a46a2f1
codesforjenuel <codesforjenuel@gmail.com>
2021-11-07 02:03:45 -0800

    first push

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// require('./routes/apiRoutes')(app);
+// require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
